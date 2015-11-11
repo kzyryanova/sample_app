900c2bddff718fbfaf56c54324112fb1637df7d6
Kate Zyryanova <katrine.zv@gmail.com>
2015-11-11 13:31:38 +0200

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
 describe "Static pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     before { visit "/static_pages/home"}
     it "should have the h1 'Sample App'" do
       expect(page).to have_selector('h1', :text => 'Sample App')
     end
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       expect(page).to have_selector('title',
-                                    :text => "Ruby on Rails Tutorial Sample App | Home",
-                                    :visible => false)
+                                :text => "Ruby on Rails Tutorial Sample App",
+                                :visible => false)
+    end
+    it "should not have a custom page title" do
+      expect(page).not_to have_selector('title', :text => '| Home', :visible => false)
     end
   end
 
@@ -21,20 +27,33 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_selector('title',
-                                    :text => "Ruby on Rails Tutorial Sample App | Help",
+                                    :text => "#{base_title} | Help",
                                     :visible => false)
     end
   end
 
   describe "About page" do
-    it "should have the h1 'Sample App'" do
+    it "should have the h1 'About'" do
       visit '/static_pages/about'
       expect(page).to have_selector('h1', :text => 'About')
     end
     it "should have the title 'About'" do
       visit '/static_pages/about'
       expect(page).to have_selector('title',
-                                    :text => "Ruby on Rails Tutorial Sample App | About",
+                                    :text => "#{base_title} | About",
+                                    :visible => false)
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector('h1', :text => 'Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector('title',
+                                    :text => "#{base_title} | Contact",
                                     :visible => false)
     end
   end
